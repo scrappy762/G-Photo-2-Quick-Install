e5de6e60fab038fa929c35e46465da000be437d0
scrappy762 <scrappy762@gmail.com>
2020-06-20 08:56:14 -0500

    This is the basic install script

---
+++ GPHOTO2-Tethering Scripts/green/downloadall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# select where create subfolders
+store_photos_in="$HOME"
+# create now variable with date values
+now=$(date +'%m-%d-%Y.%H.%M.%S')
+# just a message
+echo "Download all photos in $store_photos_in/$now"
+
+# create the folder inside store_photo path dir and make the folder name
+# as same as date
+mkdir -p "$store_photos_in/$now"
+
+
+pkill -f gphoto2
+pkill -f gphoto
+
+# go to  the new folder created and process do dowload all photos
+cd "$store_photos_in/$now" && gphoto2 --get-all-files
+
+geeqie "$store_photos_in/$now" &
